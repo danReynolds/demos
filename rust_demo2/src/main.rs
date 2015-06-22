07f41b2b476df13936bca66a44175bcad9f529cc
danReynolds <fnetma@hotmail.com>
2015-06-22 09:46:08 -0400

    updated demo 2

--- rust_demo2/src/main.rs
+++ rust_demo2/src/main.rs
@@ -1,10 +1,19 @@
 use std::process::Command;
+use std::thread;
+
 fn main() {
-    let status = Command::new("ruby").arg("test.rb").status().unwrap_or_else(|e| {
-        panic!("failed to execute process: {}", e)
+    let handle = thread::spawn(|| {
+        let status = Command::new("ruby").arg("test.rb").status().unwrap_or_else(|e| {
+            panic!("failed to execute process: {}", e)
+        });
+        "hey"
     });
-    println!("process exited with: {}", status);
-    let status = Command::new("python").arg("test.py").status().unwrap_or_else(|e| {
-        panic!("failed to execute process: {}", e)
+    println!("process exited with: {}", handle.join().unwrap());
+    let handle2 = thread::spawn(|| {
+        let status = Command::new("python").arg("test.py").status().unwrap_or_else(|e| {
+            panic!("failed to execute process: {}", e)
+        });
+        "hey2"
     });
+    println!("process exited with: {}", handle2.join().unwrap());
 }
